62d87dabb51598b6d77d29a03163d0abf735633b
Friske <gustavo.friske.avila@escola.pr.gov.br>
2024-08-15 16:24:31 -0300

    .

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(400, 400);
+  background("black");
+  }
+  function draw() {
+    
+    rect(30,20,20);
+    fill("cyan");
+    stroke("blue");
+    if(mouseIsPressed);{
+    rect(mouseX,mouseY,20,20);}}
